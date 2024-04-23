3dcf8ec39f6b93303a3b6d60ddecdadce4d80657
Benoit Moussaud <benoit@moussaud.org>
2024-04-23 17:48:39 +0200

    image prompt eng

--- src/main/java/com/broadcom/tanzu/demos/rambi/image/AzureOpenAIImageGeneratorService.java
+++ src/main/java/com/broadcom/tanzu/demos/rambi/image/AzureOpenAIImageGeneratorService.java
@@ -3,24 +3,20 @@ package com.broadcom.tanzu.demos.rambi.image;
 import com.broadcom.tanzu.demos.rambi.GeneratedRambiMovie;
 import com.broadcom.tanzu.demos.rambi.ImageGeneratorService;
 import com.broadcom.tanzu.demos.rambi.RambiConfiguration;
-
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ai.azure.openai.AzureOpenAiImageOptions;
 import org.springframework.ai.azure.openai.metadata.AzureOpenAiImageGenerationMetadata;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.image.ImageClient;
 import org.springframework.ai.image.ImageGeneration;
-
-import org.springframework.ai.image.ImageOptions;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @Profile("!fake")
@@ -34,28 +30,19 @@ public class AzureOpenAIImageGeneratorService implements ImageGeneratorService {
     @Autowired
     private ImageClient imageClient;
 
-    @Value("classpath:/movie-image-prompt-2.st")
+    @Value("classpath:/movie-image-prompt-3.st")
     private Resource moviePromptRes;
 
     @Override
     public GeneratedRambiMovie generate(GeneratedRambiMovie movie) {
         logger.info("Generate Image using AI {}", movie);
-
-        ImageOptions options = AzureOpenAiImageOptions.builder()
-                // .withQuality("hd")
-                .withStyle("vivid")
-                .withN(1)
-                .withHeight(1792)
-                .withWidth(1024)
-                .build();
-        logger.info("options {}", options);
         PromptTemplate moviePrompt = new PromptTemplate(moviePromptRes, Map.of(
                 "genre", movie.getGenre().toLowerCase(),
                 "title", movie.getTitle(),
                 "description", movie.getPosterDescription(),
                 "plot", movie.getPlot()));
         logger.info(moviePrompt.render());
-        var prompt = new ImagePrompt(moviePrompt.render(), options);
+        var prompt = new ImagePrompt(moviePrompt.render());
         movie.setImageGenerationPrompt(moviePrompt.render());
         if (configuration.failfast()) {
             movie.setPosterUrl("/images/fail_fast.png");
